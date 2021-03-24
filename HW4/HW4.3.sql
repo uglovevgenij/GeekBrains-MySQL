28334ae625c6ec62d89266c18185ae8a1aae7bca
Eugene <uglovevgenij@gmail.com>
2021-03-25 00:36:25 +0300

    up

--- HW4/HW4.3.sql
+++
@@ -1,6 +0,0 @@
-USE vk2;
-
-UPDATE media_types SET name = 'images' WHERE id = 1;
-UPDATE media_types SET name = 'audio' WHERE id = 2;
-UPDATE media_types SET name = 'video' WHERE id = 3;
-UPDATE media_types SET name = 'other' WHERE id = 4;
